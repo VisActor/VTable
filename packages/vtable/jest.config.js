fe8ca9f55f090fb6107acffcf5544ab641609525
Rui-Sun <sunrui1992@foxmail.com>
2023-06-08 20:11:53 +0800

    chore: add bugserver ci
    * chore: add bugserver ci
    * chore: update unit-test.yml

--- packages/vtable/jest.config.js
+++ packages/vtable/jest.config.js
@@ -1,18 +1,27 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
+
 module.exports = {
+  preset: 'ts-jest',
   runner: 'jest-electron/runner',
-  // testRunner: "jest-circus/runner",
-  testRegex: '/__tests__/.*\\.test\\.(js|ts|mjs)$',
   testEnvironment: 'jest-electron/environment',
-  transform: {
-    '^.+\\.(tsx|ts)?$': 'ts-jest',
-    '^.+\\.css$': 'jest-transform-stub'
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'jsx'],
+  testRegex: '/__tests__/.*\\.test\\.(js|ts)$',
+  silent: true,
   globals: {
     'ts-jest': {
-      isolatedModules: false
+      diagnostics: {
+        exclude: ['**']
+      },
+      tsconfig: {
+        resolveJsonModule: true,
+        esModuleInterop: true
+      }
     },
-    __DEV__: true,
-    __VERSION__: '1.0.0' //未生效  目前是在各个test.ts文件中分别定义了global.__VERSION__ = 'none';
-  }
+    __DEV__: true
+  },
+  verbose: true,
+  collectCoverage: true,
+  coverageReporters: ['json-summary', 'lcov'],
+  coveragePathIgnorePatterns: ['node_modules', '__tests__', 'interface.ts', '.d.ts', 'typings'],
+  setupFiles: ['./setup-mock.js']
 };
