9c0b03d33c639f5b69072e8d58ba4a8f96f18685
fangsmile <892739385@qq.com>
2023-06-15 18:05:22 +0800

    fix: columnType text change to optional

--- packages/vtable/src/ts-types/pivot-table/indicator/multilinetext-indicator.ts
+++ packages/vtable/src/ts-types/pivot-table/indicator/multilinetext-indicator.ts
@@ -3,7 +3,7 @@ import type { StylePropertyFunctionArg } from '../../style-define';
 import type { IBasicColumnIndicator, IBasicHeaderIndicator } from './basic-indicator';
 
 export interface ITextHeaderIndicator extends IBasicHeaderIndicator {
-  headerType: 'text'; // 指标表头类型
+  headerType?: 'text'; // 指标表头类型
   headerStyle?:
     | ITextStyleOption //表头可以配置吸附;
     | ((styleArg: StylePropertyFunctionArg) => ITextStyleOption); // 指标名称在表头部分显示类型
@@ -14,7 +14,7 @@ export interface ITextHeaderIndicator extends IBasicHeaderIndicator {
 }
 
 export interface ITextColumnIndicator extends IBasicColumnIndicator {
-  columnType: 'text'; // body指标值显示类型
+  columnType?: 'text'; // body指标值显示类型
   style?: ITextStyleOption | ((styleArg: StylePropertyFunctionArg) => ITextStyleOption); // body部分指标值显示样式
 
   // 目前autoWrapText和lineClamp还在style中定义
