f1977413c06248300baacd2a8658a9131bf4a958
Rui-Sun <sunrui1992@foxmail.com>
2023-07-24 20:30:13 +0800

    feat: add axes table option

--- packages/vtable/src/layout/pivot-chart/get-chart-spec.ts
+++ packages/vtable/src/layout/pivot-chart/get-chart-spec.ts
@@ -1,5 +1,7 @@
-import { cloneDeep } from '@visactor/vutils';
+import { cloneDeep, merge } from '@visactor/vutils';
 import type { PivotLayoutMap } from '../pivot-layout';
+import type { PivotChart } from '../../PivotChart';
+import type { ITableAxisOption } from '../../ts-types/component/axis';
 
 export function getRawChartSpec(col: number, row: number, layout: PivotLayoutMap): any {
   const paths = layout.getCellHeaderPaths(col, row);
@@ -38,21 +40,21 @@ export function getChartAxes(col: number, row: number, layout: PivotLayoutMap): 
       const data = layout.dataset.collectedValues[key + '_align']
         ? layout.dataset.collectedValues[key + '_align']
         : layout.dataset.collectedValues[key];
-      const range =
-        data[
-          layout.getColKeysPath()[colIndex][
-            layout.columnHeaderLevelCount - 1 - (layout.hasIndicatorAxisInColumnHeader ? 1 : 0)
-          ]
-        ];
-      axes.push({
-        type: 'linear',
-        orient: index === 0 ? 'bottom' : 'top',
-        visible: true,
-        label: { visible: false },
-        range,
-        seriesIndex: index,
-        grid: index === 0 ? undefined : { visible: false }
+      const range = data[layout.getColKeysPath()[colIndex][layout.columnHeaderLevelCount - 1 - layout.topAxesCount]];
+      const axisOption = ((layout._table as PivotChart).pivotChartAxes as ITableAxisOption[]).find(axisOption => {
+        return axisOption.orient === (index === 0 ? 'bottom' : 'top');
       });
+      axes.push(
+        merge({}, axisOption, {
+          type: 'linear',
+          orient: index === 0 ? 'bottom' : 'top',
+          // visible: true,
+          label: { visible: false },
+          title: { visible: false },
+          range,
+          seriesIndex: index
+        })
+      );
     });
 
     const rowDimensionKey = layout.getDimensionKeyInChartSpec(layout.rowHeaderLevelCount, col)[0];
@@ -60,19 +62,25 @@ export function getChartAxes(col: number, row: number, layout: PivotLayoutMap): 
     const recordRow = layout.getRecordIndexByRow(row);
     const rowPath = layout.getRowKeysPath()[recordRow];
     const domain = data[rowPath[rowPath.length - 1]] as Set<string>;
-    axes.push({
-      type: 'band',
-      orient: 'left',
-      visible: true,
-      label: { visible: false, space: 0 },
-      domainLine: { visible: false },
-      tick: { visible: false },
-      subTick: { visible: false },
-      // height: -1,
-      width: -1,
-      // autoIndent: false,
-      domain: Array.from(domain)
+    const axisOption = ((layout._table as PivotChart).pivotChartAxes as ITableAxisOption[]).find(axisOption => {
+      return axisOption.orient === 'left';
     });
+    axes.push(
+      merge({}, axisOption, {
+        type: 'band',
+        orient: 'left',
+        // visible: true,
+        label: { visible: false, space: 0 },
+        domainLine: { visible: false },
+        tick: { visible: false },
+        subTick: { visible: false },
+        title: { visible: false },
+        // height: -1,
+        width: -1,
+        // autoIndent: false,
+        domain: Array.from(domain)
+      })
+    );
   } else {
     const indicatorKeys = layout.getIndicatorKeyInChartSpec(col, row);
     const rowIndex = layout.getRecordIndexByRow(row);
@@ -80,16 +88,22 @@ export function getChartAxes(col: number, row: number, layout: PivotLayoutMap): 
       const data = layout.dataset.collectedValues[key + '_align']
         ? layout.dataset.collectedValues[key + '_align']
         : layout.dataset.collectedValues[key];
-      const range = data[layout.getRowKeysPath()[rowIndex][layout.rowHeaderLevelCount - 2]];
-      axes.push({
-        type: 'linear',
-        orient: index === 0 ? 'left' : 'right',
-        visible: true,
-        label: { visible: false },
-        range,
-        seriesIndex: index,
-        grid: index === 0 ? undefined : { visible: false }
+      const range = data[layout.getRowKeysPath()[rowIndex][layout.rowHeaderLevelCount - 1 - layout.leftAxesCount]];
+      const axisOption = ((layout._table as PivotChart).pivotChartAxes as ITableAxisOption[]).find(axisOption => {
+        return axisOption.orient === (index === 0 ? 'left' : 'right');
       });
+      axes.push(
+        merge({}, axisOption, {
+          type: 'linear',
+          orient: index === 0 ? 'left' : 'right',
+          // visible: true,
+          label: { visible: false },
+          title: { visible: false },
+          range,
+          seriesIndex: index
+          // grid: index === 0 ? undefined : { visible: false }
+        })
+      );
     });
 
     const columnDimensionKey = layout.getDimensionKeyInChartSpec(col, layout.columnHeaderLevelCount)[0];
@@ -97,18 +111,24 @@ export function getChartAxes(col: number, row: number, layout: PivotLayoutMap): 
     const recordCol = layout.getRecordIndexByCol(col);
     const colPath = layout.getColKeysPath()[recordCol];
     const domain = data[colPath[colPath.length - 1]] as Set<string>;
-    axes.push({
-      type: 'band',
-      orient: 'bottom',
-      visible: true,
-      label: { visible: false, space: 0 },
-      domainLine: { visible: false },
-      tick: { visible: false },
-      subTick: { visible: false },
-      height: -1,
-      // autoIndent: false,
-      domain: Array.from(domain)
+    const axisOption = ((layout._table as PivotChart).pivotChartAxes as ITableAxisOption[]).find(axisOption => {
+      return axisOption.orient === 'bottom';
     });
+    axes.push(
+      merge({}, axisOption, {
+        type: 'band',
+        orient: 'bottom',
+        visible: true,
+        label: { visible: false, space: 0 },
+        domainLine: { visible: false },
+        tick: { visible: false },
+        subTick: { visible: false },
+        title: { visible: false },
+        height: -1,
+        // autoIndent: false,
+        domain: Array.from(domain)
+      })
+    );
   }
   return axes;
 }
