670d3131a6e66718e3bfdad0a9e76679859db16c
Rui-Sun <sunrui1992@foxmail.com>
2023-08-15 20:11:48 +0800

    fix: add default domain in getAxisConfigInPivotChart()

--- packages/vtable/src/layout/chart-helper/get-axis-config.ts
+++ packages/vtable/src/layout/chart-helper/get-axis-config.ts
@@ -116,7 +116,7 @@ export function getAxisConfigInPivotChart(col: number, row: number, layout: Pivo
 
       const recordRow = layout.getRecordIndexByRow(row);
       const rowPath = layout.getRowKeysPath()[recordRow];
-      const domain = data[rowPath[rowPath.length - 1]] as Array<string>;
+      const domain = (data[rowPath[rowPath.length - 1]] as Array<string>) ?? [];
 
       const axisOption = getAxisOption(col, row, 'left', layout);
       if (axisOption?.visible === false) {
