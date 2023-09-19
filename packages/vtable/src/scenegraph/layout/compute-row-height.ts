1cc1387cf01d4a6724ca20195dc555533b89a7ec
fangsmile <892739385@qq.com>
2023-09-19 10:27:13 +0800

    fix: chart shake when after resize container #344

--- packages/vtable/src/scenegraph/layout/compute-row-height.ts
+++ packages/vtable/src/scenegraph/layout/compute-row-height.ts
@@ -152,7 +152,7 @@ export function computeRowsHeight(
           totalDrawHeight -
           (update
             ? newHeights.reduce((acr, cur, index) => {
-                if (index >= table.frozenRowCount && index !== newHeights.length - 1) {
+                if (index >= table.frozenRowCount && index <= table.rowCount - table.bottomFrozenRowCount - 2) {
                   return acr + cur;
                 }
                 return acr;
