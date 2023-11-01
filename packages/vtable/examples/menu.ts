6acba64976668af31b08be1b093a294071a62ba4
fangsmile <892739385@qq.com>
2023-11-01 10:18:17 +0800

    test: add more columns demo

--- packages/vtable/examples/menu.ts
+++ packages/vtable/examples/menu.ts
@@ -48,6 +48,10 @@ export const menus = [
       },
       {
         path: 'list',
+        name: 'list-more-columns'
+      },
+      {
+        path: 'list',
         name: 'list-no-columns'
       }
     ]
