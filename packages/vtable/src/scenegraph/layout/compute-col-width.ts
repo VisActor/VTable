1164c81ac7b20ba633792013b1a270068c62d116
Rui-Sun <sunrui1992@foxmail.com>
2023-12-01 17:02:30 +0800

    fix: fix custom render renderDefault auto size problem

--- packages/vtable/src/scenegraph/layout/compute-col-width.ts
+++ packages/vtable/src/scenegraph/layout/compute-col-width.ts
@@ -316,9 +316,11 @@ function computeAutoColWidth(
 
     // 判断CustomRender
     const customWidth = computeCustomRenderWidth(col, row, table);
-    if (typeof customWidth === 'number') {
-      maxWidth = Math.max(customWidth, maxWidth);
-      continue;
+    if (customWidth) {
+      maxWidth = Math.max(customWidth.width, maxWidth);
+      if (!customWidth.renderDefault) {
+        continue;
+      }
     }
 
     // 判断透视表如果在指标
@@ -402,12 +404,13 @@ function computeAutoColWidth(
  * @param {BaseTableAPI} table
  * @return {*}
  */
-function computeCustomRenderWidth(col: number, row: number, table: BaseTableAPI): number | undefined {
+function computeCustomRenderWidth(col: number, row: number, table: BaseTableAPI) {
   const customRender = table.getCustomRender(col, row);
   const customLayout = table.getCustomLayout(col, row);
   if (customRender || customLayout) {
     let spanCol = 1;
     let width = 0;
+    let renderDefault = false;
     if (table.isHeader(col, row) || (table.getBodyColumnDefine(col, row) as TextColumnDefine)?.mergeCell) {
       const cellRange = table.getCellRange(col, row);
       spanCol = cellRange.end.col - cellRange.start.col + 1;
@@ -428,11 +431,7 @@ function computeCustomRenderWidth(col: number, row: number, table: BaseTableAPI)
         dealPercentCalc(customLayoutObj.rootContainer, 0, table.getRowHeight(row));
         customLayoutObj.rootContainer.setStage(table.scenegraph.stage);
         width = (customLayoutObj.rootContainer as VGroup).AABBBounds.width() ?? 0;
-        // width = (customLayoutObj.rootContainer as VGroup).attribute.width ?? 0;
-        // } else if (customLayoutObj.rootContainer) {
-        //   customLayoutObj.rootContainer.isRoot = true;
-        //   const size = customLayoutObj.rootContainer.getContentSize();
-        //   width = size.width ?? 0;
+        renderDefault = customLayoutObj.renderDefault;
       } else {
         width = 0;
       }
@@ -440,10 +439,15 @@ function computeCustomRenderWidth(col: number, row: number, table: BaseTableAPI)
       // 处理customRender
       const customRenderObj = customRender(arg);
       width = customRenderObj?.expectedWidth ?? 0;
+      renderDefault = customRenderObj?.renderDefault;
     } else {
       width = customRender?.expectedWidth ?? 0;
+      renderDefault = customRender?.renderDefault;
     }
-    return width / spanCol;
+    return {
+      width: width / spanCol,
+      renderDefault
+    };
   }
   return undefined;
 }
