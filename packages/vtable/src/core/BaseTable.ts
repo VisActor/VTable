19b26eecccbf71b82d1c1b6d61042d3830d785f9
fangsmile <892739385@qq.com>
2024-01-06 08:57:19 +0800

    fix: set display:none trigger resize logic

--- packages/vtable/src/core/BaseTable.ts
+++ packages/vtable/src/core/BaseTable.ts
@@ -768,8 +768,8 @@ export abstract class BaseTable extends EventTarget implements BaseTableAPI {
     if (Env.mode === 'browser') {
       const element = this.getElement();
 
-      const width1 = (element.offsetWidth || element.parentElement?.offsetWidth || 1) - 1;
-      const height1 = (element.offsetHeight || element.parentElement?.offsetHeight || 1) - 1;
+      const width1 = (element.parentElement?.offsetWidth ?? 1) - 1;
+      const height1 = (element.parentElement?.offsetHeight ?? 1) - 1;
 
       element.style.width = (width1 && `${width1 - padding.left - padding.right}px`) || '0px';
       element.style.height = (height1 && `${height1 - padding.top - padding.bottom}px`) || '0px';
