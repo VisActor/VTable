09ef8c5e983cb5bc35a2d6442e7185c6cea6a8a8
Rui-Sun <sunrui1992@foxmail.com>
2024-01-22 14:57:37 +0800

    feat: add enlarge in calcKeepAspectRatioSize

--- packages/vtable/src/scenegraph/utils/keep-aspect-ratio.ts
+++ packages/vtable/src/scenegraph/utils/keep-aspect-ratio.ts
@@ -1,22 +1,52 @@
 export function calcKeepAspectRatioSize(
-  width: number,
-  height: number,
-  maxWidth: number,
-  maxHeight: number
+  width: number, // image width
+  height: number, // image height
+  maxWidth: number, // cell width
+  maxHeight: number // cell height
 ): {
   width: number;
   height: number;
 } {
-  let newWidth = width;
-  let newHeight = height;
-  if (newWidth > maxWidth) {
-    newWidth = maxWidth;
-    newHeight = (newWidth * height) / width;
+  // let newWidth = width;
+  // let newHeight = height;
+  // if (newWidth > maxWidth) {
+  //   newWidth = maxWidth;
+  //   newHeight = (newWidth * height) / width;
+  // }
+  // if (newHeight > maxHeight) {
+  //   newHeight = maxHeight;
+  //   newWidth = (newHeight * width) / height;
+  // }
+  // return {
+  //   width: newWidth,
+  //   height: newHeight
+  // };
+
+  const rectWidth = width;
+  const rectHeight = height;
+  const containerWidth = maxWidth;
+  const containerHeight = maxHeight;
+  const containerRatio = containerWidth / containerHeight;
+  const rectRatio = rectWidth / rectHeight;
+  let newWidth;
+  let newHeight;
+  let offsetX;
+  let offsetY;
+
+  if (rectRatio > containerRatio) {
+    // 矩形的宽高比较大，以容器的宽度为基准进行缩放
+    newWidth = containerWidth;
+    newHeight = newWidth / rectRatio;
+    offsetX = 0;
+    offsetY = (containerHeight - newHeight) / 2;
+  } else {
+    // 矩形的高宽比较大，以容器的高度为基准进行缩放
+    newHeight = containerHeight;
+    newWidth = newHeight * rectRatio;
+    offsetY = 0;
+    offsetX = (containerWidth - newWidth) / 2;
   }
-  if (newHeight > maxHeight) {
-    newHeight = maxHeight;
-    newWidth = (newHeight * width) / height;
-  }
+
   return {
     width: newWidth,
     height: newHeight
