f082e3db6105ea392cb206b4adb3589bd6b3cb87
Rui-Sun <sunrui1992@foxmail.com>
2024-02-06 16:19:11 +0800

    fix: fix custom merge cell computed height

--- packages/vtable/examples/list/list-merge-cell.ts
+++ packages/vtable/examples/list/list-merge-cell.ts
@@ -46,7 +46,7 @@ const dataSource = new VTable.data.CachedDataSource({
 });
 
 export function createTable() {
-  // const records = generatePersons(1000);
+  const records = new Array(1000).fill(generatePersons(1000));
   const columns: VTable.ColumnsDefine = [
     {
       field: 'id',
@@ -104,15 +104,15 @@ export function createTable() {
   ];
   const option = {
     container: document.getElementById(CONTAINER_ID),
-    // records,
+    records,
     columns,
     tooltip: {
       isShowOverflowTextTooltip: true
     },
     allowFrozenColCount: 3,
     // frozenColCount: 2,
-    // autoWrapText: true,
-    // heightMode: 'autoHeight',
+    autoWrapText: true,
+    heightMode: 'autoHeight',
     // widthMode: 'adaptive',
     customMergeCell: (col, row, table) => {
       if (col > 0 && col < 8 && row > 7 && row < 11) {
@@ -132,141 +132,142 @@ export function createTable() {
             bgColor: '#fff'
           }
         };
-      } else if (col > 0 && col < 3 && row > 1 && row < 4) {
-        return {
-          range: {
-            start: {
-              col: 1,
-              row: 2
-            },
-            end: {
-              col: 2,
-              row: 3
-            }
-          },
-          customLayout: args => {
-            const { table, row, col, rect } = args;
-            const record = {
-              bloggerId: 1,
-              bloggerName: 'Virtual Anchor Xiaohua',
-              bloggerAvatar: 'https://lf9-dp-fe-cms-tos.byteorg.com/obj/bit-cloud/VTable/custom-render/flower.jpg',
-              fansCount: 400,
-              worksCount: 10,
-              viewCount: 5,
-              city: 'Dream City',
-              tags: ['game', 'anime', 'food']
-            };
-            const { height, width } = rect ?? table.getCellRect(col, row);
-            const percentCalc = VTable.CustomLayout.percentCalc;
-
-            const container = new VTable.CustomLayout.Group({
-              height,
-              width,
-              display: 'flex',
-              flexDirection: 'row',
-              flexWrap: 'nowrap'
-            });
-            const containerLeft = new VTable.CustomLayout.Group({
-              height: percentCalc(100),
-              width: 60,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-              justifyContent: 'space-around'
-            });
-            container.add(containerLeft);
-
-            const icon0 = new VTable.CustomLayout.Image({
-              id: 'icon0',
-              width: 50,
-              height: 50,
-              image: record.bloggerAvatar,
-              cornerRadius: 25
-            });
-            containerLeft.add(icon0);
-
-            const containerRight = new VTable.CustomLayout.Group({
-              height: percentCalc(100),
-              width: percentCalc(100, -60),
-              display: 'flex',
-              flexDirection: 'column',
-              flexWrap: 'nowrap'
-            });
-            container.add(containerRight);
-
-            const containerRightTop = new VTable.CustomLayout.Group({
-              height: percentCalc(50),
-              width: percentCalc(100),
-              display: 'flex',
-              alignItems: 'flex-end'
-            });
-
-            const containerRightBottom = new VTable.CustomLayout.Group({
-              height: percentCalc(50),
-              width: percentCalc(100),
-              display: 'flex',
-              alignItems: 'center'
-            });
-
-            containerRight.add(containerRightTop);
-            containerRight.add(containerRightBottom);
-
-            const bloggerName = new VTable.CustomLayout.Text({
-              text: record.bloggerName,
-              fontSize: 13,
-              fontFamily: 'sans-serif',
-              fill: 'black'
-            });
-            containerRightTop.add(bloggerName);
-
-            const location = new VTable.CustomLayout.Image({
-              id: 'location',
-              image: 'https://lf9-dp-fe-cms-tos.byteorg.com/obj/bit-cloud/VTable/location.svg',
-              width: 15,
-              height: 15,
-              boundsPadding: [0, 0, 0, 10]
-            });
-            containerRightTop.add(location);
-
-            const locationName = new VTable.CustomLayout.Text({
-              text: record.city,
-              fontSize: 11,
-              fontFamily: 'sans-serif',
-              fill: '#6f7070'
-            });
-            containerRightTop.add(locationName);
-
-            for (let i = 0; i < record?.tags?.length ?? 0; i++) {
-              const tag = new VTable.CustomLayout.Tag({
-                text: record.tags[i],
-                textStyle: {
-                  fontSize: 10,
-                  fontFamily: 'sans-serif',
-                  fill: 'rgb(51, 101, 238)'
-                },
-                panel: {
-                  visible: true,
-                  fill: '#f4f4f2',
-                  cornerRadius: 5
-                },
-                space: 5,
-                boundsPadding: [0, 0, 0, 5]
-              });
-              containerRightBottom.add(tag);
-            }
-            return {
-              rootContainer: container,
-              renderDefault: false
-            };
-          }
-        };
       }
+      // else if (col > 0 && col < 3 && row > 1 && row < 4) {
+      //   return {
+      //     range: {
+      //       start: {
+      //         col: 1,
+      //         row: 2
+      //       },
+      //       end: {
+      //         col: 2,
+      //         row: 3
+      //       }
+      //     },
+      //     customLayout: args => {
+      //       const { table, row, col, rect } = args;
+      //       const record = {
+      //         bloggerId: 1,
+      //         bloggerName: 'Virtual Anchor Xiaohua',
+      //         bloggerAvatar: 'https://lf9-dp-fe-cms-tos.byteorg.com/obj/bit-cloud/VTable/custom-render/flower.jpg',
+      //         fansCount: 400,
+      //         worksCount: 10,
+      //         viewCount: 5,
+      //         city: 'Dream City',
+      //         tags: ['game', 'anime', 'food']
+      //       };
+      //       const { height, width } = rect ?? table.getCellRect(col, row);
+      //       const percentCalc = VTable.CustomLayout.percentCalc;
+
+      //       const container = new VTable.CustomLayout.Group({
+      //         height,
+      //         width,
+      //         display: 'flex',
+      //         flexDirection: 'row',
+      //         flexWrap: 'nowrap'
+      //       });
+      //       const containerLeft = new VTable.CustomLayout.Group({
+      //         height: percentCalc(100),
+      //         width: 60,
+      //         display: 'flex',
+      //         flexDirection: 'column',
+      //         alignItems: 'center',
+      //         justifyContent: 'space-around'
+      //       });
+      //       container.add(containerLeft);
+
+      //       const icon0 = new VTable.CustomLayout.Image({
+      //         id: 'icon0',
+      //         width: 50,
+      //         height: 50,
+      //         image: record.bloggerAvatar,
+      //         cornerRadius: 25
+      //       });
+      //       containerLeft.add(icon0);
+
+      //       const containerRight = new VTable.CustomLayout.Group({
+      //         height: percentCalc(100),
+      //         width: percentCalc(100, -60),
+      //         display: 'flex',
+      //         flexDirection: 'column',
+      //         flexWrap: 'nowrap'
+      //       });
+      //       container.add(containerRight);
+
+      //       const containerRightTop = new VTable.CustomLayout.Group({
+      //         height: percentCalc(50),
+      //         width: percentCalc(100),
+      //         display: 'flex',
+      //         alignItems: 'flex-end'
+      //       });
+
+      //       const containerRightBottom = new VTable.CustomLayout.Group({
+      //         height: percentCalc(50),
+      //         width: percentCalc(100),
+      //         display: 'flex',
+      //         alignItems: 'center'
+      //       });
+
+      //       containerRight.add(containerRightTop);
+      //       containerRight.add(containerRightBottom);
+
+      //       const bloggerName = new VTable.CustomLayout.Text({
+      //         text: record.bloggerName,
+      //         fontSize: 13,
+      //         fontFamily: 'sans-serif',
+      //         fill: 'black'
+      //       });
+      //       containerRightTop.add(bloggerName);
+
+      //       const location = new VTable.CustomLayout.Image({
+      //         id: 'location',
+      //         image: 'https://lf9-dp-fe-cms-tos.byteorg.com/obj/bit-cloud/VTable/location.svg',
+      //         width: 15,
+      //         height: 15,
+      //         boundsPadding: [0, 0, 0, 10]
+      //       });
+      //       containerRightTop.add(location);
+
+      //       const locationName = new VTable.CustomLayout.Text({
+      //         text: record.city,
+      //         fontSize: 11,
+      //         fontFamily: 'sans-serif',
+      //         fill: '#6f7070'
+      //       });
+      //       containerRightTop.add(locationName);
+
+      //       for (let i = 0; i < record?.tags?.length ?? 0; i++) {
+      //         const tag = new VTable.CustomLayout.Tag({
+      //           text: record.tags[i],
+      //           textStyle: {
+      //             fontSize: 10,
+      //             fontFamily: 'sans-serif',
+      //             fill: 'rgb(51, 101, 238)'
+      //           },
+      //           panel: {
+      //             visible: true,
+      //             fill: '#f4f4f2',
+      //             cornerRadius: 5
+      //           },
+      //           space: 5,
+      //           boundsPadding: [0, 0, 0, 5]
+      //         });
+      //         containerRightBottom.add(tag);
+      //       }
+      //       return {
+      //         rootContainer: container,
+      //         renderDefault: false
+      //       };
+      //     }
+      //   };
+      // }
     }
   };
   const tableInstance = new VTable.ListTable(option);
   window.tableInstance = tableInstance;
 
-  tableInstance.dataSource = dataSource;
+  // tableInstance.dataSource = dataSource;
 
   bindDebugTool(tableInstance.scenegraph.stage, { customGrapicKeys: ['col', 'row'] });
   // tableInstance.on('sort_click', args => {
