96b1d42f29754ce19907fcfb964bdc77dddbf27b
Rui-Sun <sunrui1992@foxmail.com>
2024-02-27 10:55:16 +0800

    fix: reset theme rowHeaderStyle config

--- packages/vtable/src/themes/theme.ts
+++ packages/vtable/src/themes/theme.ts
@@ -376,7 +376,7 @@ export class TableTheme implements ITableThemeDefine {
         {},
         this.defaultStyle,
         superTheme.rowHeaderStyle,
-        obj.rowHeaderStyle // ?? obj.headerStyle
+        obj.rowHeaderStyle ?? obj.headerStyle
       );
       this._rowHeader = this.getStyle(header);
     }
