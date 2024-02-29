889c4c2e8cc42896b4014cbc9a4b8338a0465116
Rui-Sun <sunrui1992@foxmail.com>
2024-02-29 17:56:01 +0800

    fix: fix col & row unlegel in getDefaultHeight() & getDefaultWidth()

--- packages/vtable/src/scenegraph/group-creater/progress/default-width-height.ts
+++ packages/vtable/src/scenegraph/group-creater/progress/default-width-height.ts
@@ -7,15 +7,15 @@ const rowSamplingNumber = 5;
 // The default row height and column width for proxy
 // use sampling calculation to get closer to the actual situation
 export function getDefaultWidth(table: BaseTableAPI) {
-  const { rowCount, colCount, frozenColCount, frozenRowCount, rightFrozenColCount, bottomFrozenRowCount } = table;
+  const { rowCount, colCount } = table;
 
   const widths = [];
   // const heights = [];
-  const deltaCol = Math.ceil((rowCount - frozenRowCount - bottomFrozenRowCount) / colSamplingNumber);
-  const deltaRow = Math.ceil((colCount - frozenColCount - rightFrozenColCount) / rowSamplingNumber);
+  const deltaCol = Math.max(1, Math.ceil(rowCount / colSamplingNumber));
+  const deltaRow = Math.max(1, Math.ceil(colCount / rowSamplingNumber));
 
-  for (let col = frozenColCount; col < colCount; col += deltaCol) {
-    for (let row = frozenRowCount; row < rowCount; row += deltaRow) {
+  for (let col = 0; col < colCount; col += deltaCol) {
+    for (let row = 0; row < rowCount; row += deltaRow) {
       widths.push(computeColWidth(col, row, row, table));
       // heights.push(computeRowHeight(row, col, col, table));
     }
@@ -27,15 +27,15 @@ export function getDefaultWidth(table: BaseTableAPI) {
 }
 
 export function getDefaultHeight(table: BaseTableAPI) {
-  const { rowCount, colCount, frozenColCount, frozenRowCount, rightFrozenColCount, bottomFrozenRowCount } = table;
+  const { rowCount, colCount } = table;
 
   // const widths = [];
   const heights = [];
-  const deltaCol = Math.ceil((rowCount - frozenRowCount - bottomFrozenRowCount) / colSamplingNumber);
-  const deltaRow = Math.ceil((colCount - frozenColCount - rightFrozenColCount) / rowSamplingNumber);
+  const deltaCol = Math.max(1, Math.ceil(rowCount / colSamplingNumber));
+  const deltaRow = Math.max(1, Math.ceil(colCount / rowSamplingNumber));
 
-  for (let col = frozenColCount; col < colCount; col += deltaCol) {
-    for (let row = frozenRowCount; row < rowCount; row += deltaRow) {
+  for (let col = 0; col < colCount; col += deltaCol) {
+    for (let row = 0; row < rowCount; row += deltaRow) {
       // widths.push(computeColWidth(col, row, row, table));
       heights.push(computeRowHeight(row, col, col, table));
     }
