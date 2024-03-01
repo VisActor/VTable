359375058cff379884476279c4270baf93fb3586
fangsmile <892739385@qq.com>
2024-03-01 15:29:52 +0800

    docs: update changlog of rush

--- packages/vtable/examples/list/list.ts
+++ packages/vtable/examples/list/list.ts
@@ -1,196 +1,48 @@
 import * as VTable from '../../src';
 import { bindDebugTool } from '../../src/scenegraph/debug-tool';
 const CONTAINER_ID = 'vTable';
-const generatePersons = count => {
-  return Array.from(new Array(count)).map((_, i) => ({
-    id: i + 1,
-    email1: `${i + 1}@xxx.com`,
-    name: `小明${i + 1}`,
-    lastName: '王',
-    date1: '2022年9月1日',
-    tel: '000-0000-0000',
-    sex: i % 2 === 0 ? 'boy' : 'girl',
-    work: i % 2 === 0 ? 'back-end engineer' + (i + 1) : 'front-end engineer' + (i + 1),
-    city: 'beijing'
-  }));
-};
-
 export function createTable() {
-  const records = generatePersons(10000);
-  const columns: VTable.ColumnsDefine = [
+  const records = [
     {
-      field: '',
-      title: '行号',
-      width: 80,
-      fieldFormat(data, col, row, table) {
-        return row - 1;
-      }
-    },
-    {
-      field: 'id',
-      title: 'ID',
-      width: 'auto',
-      minWidth: 50,
-      sort: true
-    },
-    {
-      field: 'email1',
-      title: 'email',
-      width: 200,
-      sort: true
-    },
-    {
-      title: 'full name',
-      columns: [
-        {
-          field: 'name',
-          title: 'First Name',
-          width: 200
-        },
-        {
-          field: 'name',
-          title: 'Last Name',
-          width: 200
-        }
-      ]
-    },
-    {
-      field: 'date1',
-      title: 'birthday',
-      width: 200
-    },
-    {
-      field: 'sex',
-      title: 'sex',
-      width: 100
-    },
-    {
-      field: 'tel',
-      title: 'telephone',
-      width: 150
-    },
-    {
-      field: 'work',
-      title: 'job',
-      width: 200
-    },
-    {
-      field: 'city',
-      title: 'city',
-      width: 150
-    },
-    {
-      field: 'date1',
-      title: 'birthday',
-      width: 200
-    },
-    {
-      field: 'sex',
-      title: 'sex',
-      width: 100
-    },
-    {
-      field: 'tel',
-      title: 'telephone',
-      width: 150
-    },
-    {
-      field: 'work',
-      title: 'job',
-      width: 200
-    },
-    {
-      field: 'city',
-      title: 'city',
-      width: 150
-    },
-    {
-      field: 'date1',
-      title: 'birthday',
-      width: 200
-    },
-    {
-      field: 'sex',
-      title: 'sex',
-      width: 100
-    },
-    {
-      field: 'tel',
-      title: 'telephone',
-      width: 150
-    },
-    {
-      field: 'work',
-      title: 'job',
-      width: 200
-    },
-    {
-      field: 'city',
-      title: 'city',
-      width: 150
-    },
-    {
-      field: 'date1',
-      title: 'birthday',
-      width: 200
-    },
-    {
-      field: 'sex',
-      title: 'sex',
-      width: 100
-    },
-    {
-      field: 'tel',
-      title: 'telephone',
-      width: 150
-    },
-    {
-      field: 'work',
-      title: 'job',
-      width: 200
-    },
-    {
-      field: 'city',
-      title: 'city',
-      width: 150
+      '230517143221027': 'CA-2018-156720',
+      '230517143221030': 'JM-15580',
+      '230517143221032': 'Bagged Rubber Bands',
+      '230517143221023': 'Office Supplies',
+      '230517143221034': 'Fasteners',
+      '230517143221037': 'West',
+      '230517143221024': 'Loveland',
+      '230517143221029': '2018-12-30',
+      '230517143221042': '3',
+      '230517143221040': '3.024',
+      '230517143221041': '-0.605'
     }
   ];
+
+  const columns = [
+    {
+      field: '230517143221027',
+      title: 'Order ID',
+      width: 'auto',
+      sort: true
+    },
+    {
+      field: '230517143221027',
+      title: 'Order ID11 2',
+      width: 'auto',
+      sort: true
+    }
+  ];
+
   const option: VTable.ListTableConstructorOptions = {
-    container: document.getElementById(CONTAINER_ID),
     records,
     columns,
-    tooltip: {
-      isShowOverflowTextTooltip: true
-    },
+    widthMode: 'standard',
     frozenColCount: 1,
-    bottomFrozenRowCount: 2,
     rightFrozenColCount: 2,
-    overscrollBehavior: 'none',
-    dragHeaderMode: 'all',
-    keyboardOptions: {
-      pasteValueToCell: true
-    },
-    eventOptions: {
-      preventDefaultContextMenu: false
-    }
-    // widthMode: 'adaptive'
+    bottomFrozenRowCount: 1
   };
-  const tableInstance = new VTable.ListTable(option);
+
+  // 创建 VTable 实例
+  const tableInstance = new VTable.ListTable(document.getElementById(CONTAINER_ID), option);
   window.tableInstance = tableInstance;
-  tableInstance.on('change_cell_value', arg => {
-    console.log(arg);
-  });
-  // setTimeout(() => {
-  //   tableInstance.addRecord({ id: 333 }, 6);
-  // }, 3000);
-  // tableInstance.on('sort_click', args => {
-  //   tableInstance.updateSortState(
-  //     {
-  //       field: args.field,
-  //       order: Date.now() % 3 === 0 ? 'desc' : Date.now() % 3 === 1 ? 'asc' : 'normal'
-  //     },
-  //     false
-  //   );
-  //   return false; //return false代表不执行内部排序逻辑
-  // });
 }
