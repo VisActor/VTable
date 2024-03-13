ebbe601c08f49ef345a4a2f18bc5cd4271d39a3f
fangsmile <892739385@qq.com>
2024-03-13 20:07:58 +0800

    fix: rowHeaderColWidth exceed large value set frozenColCount to zero

--- packages/vtable/src/state/state.ts
+++ packages/vtable/src/state/state.ts
@@ -599,7 +599,7 @@ export class StateManager {
     const originalFrozenColCount =
       this.table.isListTable() && !this.table.internalProps.transpose
         ? this.table.options.frozenColCount
-        : Math.max(this.table.rowHeaderLevelCount, this.table.options.frozenColCount);
+        : Math.max(this.table.rowHeaderLevelCount ?? 0, this.table.options.frozenColCount ?? 0);
     if (originalFrozenColCount) {
       if (this.table.tableNoFrameWidth - this.table.getColsWidth(0, originalFrozenColCount - 1) <= 120) {
         this.table._setFrozenColCount(0);
