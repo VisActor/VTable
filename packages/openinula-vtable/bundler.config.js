a33cbaac44a6e5118c4ff163967b23d701b417c7
Rui-Sun <sunrui1992@foxmail.com>
2024-03-18 16:17:31 +0800

    chore: change bunder config in openinula-vtable

--- packages/openinula-vtable/bundler.config.js
+++ packages/openinula-vtable/bundler.config.js
@@ -2,10 +2,10 @@
  * @type {Partial<import('@internal/bundler').Config>}
  */
 module.exports = {
-  formats: ['cjs', 'es', 'umd'],
+  formats: ['cjs', 'es'],
   noEmitOnError: false,
   copy: ['css'],
-  name: 'ReactVTable',
+  name: 'InulaVTable',
   umdOutputFilename: 'openinula-vtable',
   rollupOptions: {
     treeshake: true
@@ -13,7 +13,7 @@ module.exports = {
   globals: {
     '@visactor/vtable': 'VTable'
   },
-  external: ['@visactor/vtable'],
+  external: ['@visactor/vtable', 'openinula'],
   alias: {
     react: 'openinula', // 新增
     'react-dom': 'openinula', // 新增
