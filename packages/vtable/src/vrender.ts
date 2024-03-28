af5f5424c386be477c9aeaec2f6cdd8eb665e8c4
Rui-Sun <sunrui1992@foxmail.com>
2024-03-28 14:57:59 +0800

    chore: change build funtion for vtable-used-in-vrender-bugserver

--- packages/vtable/src/vrender.ts
+++ packages/vtable/src/vrender.ts
@@ -1,62 +1,69 @@
-import '@visactor/vrender-core';
-import { container, isBrowserEnv, isNodeEnv, preLoadAllModule } from '@visactor/vrender-core';
-import { loadBrowserEnv, loadNodeEnv } from '@visactor/vrender-kits';
-import {
-  registerArc,
-  registerArc3d,
-  registerArea,
-  registerCircle,
-  registerGlyph,
-  registerGroup,
-  registerImage,
-  registerLine,
-  registerPath,
-  registerPolygon,
-  registerPyramid3d,
-  registerRect,
-  registerRect3d,
-  registerRichtext,
-  registerShadowRoot,
-  registerSymbol,
-  registerText,
-  registerWrapText
-} from '@visactor/vrender-kits';
-// 导出版本号
-// export const version = __VERSION__;
+// import '@visactor/vrender-core';
+// import { container, isBrowserEnv, isNodeEnv, preLoadAllModule } from '@visactor/vrender-core';
+// import { loadBrowserEnv, loadNodeEnv } from '@visactor/vrender-kits';
+// import {
+//   registerArc,
+//   registerArc3d,
+//   registerArea,
+//   registerCircle,
+//   registerGlyph,
+//   registerGroup,
+//   registerImage,
+//   registerLine,
+//   registerPath,
+//   registerPolygon,
+//   registerPyramid3d,
+//   registerRect,
+//   registerRect3d,
+//   registerRichtext,
+//   registerShadowRoot,
+//   registerSymbol,
+//   registerText,
+//   registerWrapText
+// } from '@visactor/vrender-kits';
+// // 导出版本号
+// // export const version = __VERSION__;
 
-let registed = false;
+// let registed = false;
+// export function registerForVrender() {
+//   if (registed) {
+//     return;
+//   }
+//   registed = true;
+//   // 注册内置组件
+//   preLoadAllModule();
+
+//   if (isBrowserEnv()) {
+//     loadBrowserEnv(container);
+//   } else if (isNodeEnv()) {
+//     loadNodeEnv(container);
+//   }
+//   registerArc();
+//   registerArc3d();
+//   registerArea();
+//   registerCircle();
+//   registerGlyph();
+//   registerGroup();
+//   registerImage();
+//   registerLine();
+//   registerPath();
+//   registerPolygon();
+//   registerPyramid3d();
+//   registerRect();
+//   registerRect3d();
+//   registerRichtext();
+//   registerShadowRoot();
+//   registerSymbol();
+//   registerText();
+//   registerWrapText();
+// }
+
+// export * from '@visactor/vrender-core';
+// export * from '@visactor/vrender-kits';
+
+// only for vtable-used-in-vrender-bugserver.js
+export * from '@visactor/vrender';
+
 export function registerForVrender() {
-  if (registed) {
-    return;
-  }
-  registed = true;
-  // 注册内置组件
-  preLoadAllModule();
-
-  if (isBrowserEnv()) {
-    loadBrowserEnv(container);
-  } else if (isNodeEnv()) {
-    loadNodeEnv(container);
-  }
-  registerArc();
-  registerArc3d();
-  registerArea();
-  registerCircle();
-  registerGlyph();
-  registerGroup();
-  registerImage();
-  registerLine();
-  registerPath();
-  registerPolygon();
-  registerPyramid3d();
-  registerRect();
-  registerRect3d();
-  registerRichtext();
-  registerShadowRoot();
-  registerSymbol();
-  registerText();
-  registerWrapText();
+  // do nothing
 }
-
-export * from '@visactor/vrender-core';
-export * from '@visactor/vrender-kits';
