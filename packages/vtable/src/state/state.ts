9c63461c5e377ab4c5b57d2d95419049918bf44c
Rui-Sun <sunrui1992@foxmail.com>
2024-04-15 20:46:49 +0800

    fix: fix check box state init

--- packages/vtable/src/state/state.ts
+++ packages/vtable/src/state/state.ts
@@ -1275,6 +1275,7 @@ export class StateManager {
   initCheckedState(records: any[]) {
     // clear checkbox state
     this.checkedState = [];
+    this.headerCheckedState = {};
     this.radioState = {};
 
     let isNeedInitHeaderCheckedStateFromRecord = false;
