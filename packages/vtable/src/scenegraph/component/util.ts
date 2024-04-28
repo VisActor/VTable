3ef1cba5e1b2d661251eee5b0d337c92b757870b
Rui-Sun <sunrui1992@foxmail.com>
2024-04-28 12:13:32 +0800

    fix: fix table size in frozen position calculation

--- packages/vtable/src/scenegraph/component/util.ts
+++ packages/vtable/src/scenegraph/component/util.ts
@@ -2,7 +2,7 @@ import type { BaseTableAPI } from '../../ts-types/base-table';
 
 export function getColX(col: number, table: BaseTableAPI, isRightFrozen?: boolean) {
   if (isRightFrozen) {
-    return table.tableNoFrameWidth - table.getColsWidth(col, table.colCount - 1);
+    return Math.min(table.tableNoFrameWidth, table.getAllColsWidth()) - table.getColsWidth(col, table.colCount - 1);
   }
   let colX = table.getColsWidth(0, col);
   if (col >= table.frozenColCount) {
@@ -13,7 +13,7 @@ export function getColX(col: number, table: BaseTableAPI, isRightFrozen?: boolea
 
 export function getRowY(row: number, table: BaseTableAPI, isBottomFrozen?: boolean) {
   if (isBottomFrozen) {
-    return table.tableNoFrameWidth - table.getRowsHeight(row, table.rowCount - 1);
+    return Math.min(table.tableNoFrameWidth, table.getAllRowsHeight()) - table.getRowsHeight(row, table.rowCount - 1);
   }
   let rowY = table.getRowsHeight(0, row);
   if (row >= table.frozenRowCount) {
