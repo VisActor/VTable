3e26653bffd7307c5127235c148f9b2625c96541
Rui-Sun <sunrui1992@foxmail.com>
2024-05-23 14:38:01 +0800

    fix: use table option in hasAutoImageColumn()

--- packages/vtable/src/layout/layout-helper.ts
+++ packages/vtable/src/layout/layout-helper.ts
@@ -1,6 +1,11 @@
-import type { Aggregation } from '../ts-types';
+import { isString } from '@visactor/vutils';
+import type { Aggregation, ListTableConstructorOptions, PivotTableConstructorOptions } from '../ts-types';
+import type { BaseTableAPI } from '../ts-types/base-table';
 import type { ColumnData } from '../ts-types/list-table/layout-map/api';
 import type { SimpleHeaderLayoutMap } from './simple-header-layout';
+import type { IImageDimension } from '../ts-types/pivot-table/dimension/image-dimension';
+import type { IImageColumnIndicator, IImageHeaderIndicator } from '../ts-types/pivot-table/indicator/image-indicator';
+import type { IImageColumnBodyDefine, IImageHeaderDefine } from '../ts-types/list-table/define/image-define';
 
 export function checkHasAggregation(layoutMap: SimpleHeaderLayoutMap) {
   const columnObjects = layoutMap.columnObjects;
@@ -52,11 +57,89 @@ export function checkHasAggregationOnBottom(layoutMap: SimpleHeaderLayoutMap) {
 }
 
 export function checkHasTreeDefine(layoutMap: SimpleHeaderLayoutMap) {
-  const columnObjects = layoutMap.columnObjects;
-  for (let i = 0; i < columnObjects.length; i++) {
-    const column = columnObjects[i];
-    if ((column as ColumnData)?.define?.tree) {
-      return true;
+  const { columns } = layoutMap._table.options as ListTableConstructorOptions;
+  if (columns.length > 0) {
+    for (let i = 0; i < columns.length; i++) {
+      const column = columns[i];
+      if (isString(column)) {
+        continue;
+      }
+      if (column.tree) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+export function hasAutoImageColumn(table: BaseTableAPI) {
+  const { columns, rows, indicators } = table.options as PivotTableConstructorOptions;
+  if (table.isPivotTable()) {
+    // pivot table
+    if (columns.length > 0) {
+      for (let i = 0; i < columns.length; i++) {
+        const column = columns[i];
+        if (isString(column)) {
+          continue;
+        }
+        if (
+          (column.headerType === 'image' || column.headerType === 'video' || typeof column.headerType === 'function') &&
+          (column as IImageDimension).imageAutoSizing
+        ) {
+          return true;
+        }
+      }
+    }
+    if (rows.length > 0) {
+      for (let i = 0; i < rows.length; i++) {
+        const row = rows[i];
+        if (isString(row)) {
+          continue;
+        }
+        if (
+          (row.headerType === 'image' || row.headerType === 'video' || typeof row.headerType === 'function') &&
+          (row as IImageDimension).imageAutoSizing
+        ) {
+          return true;
+        }
+      }
+    }
+    if (indicators.length > 0) {
+      for (let i = 0; i < indicators.length; i++) {
+        const indicator = indicators[i];
+        if (isString(indicator)) {
+          continue;
+        }
+        if (
+          ((indicator.cellType === 'image' ||
+            indicator.cellType === 'video' ||
+            typeof indicator.cellType === 'function') &&
+            (indicator as IImageColumnIndicator).imageAutoSizing) ||
+          ((indicator.headerType === 'image' ||
+            indicator.headerType === 'video' ||
+            typeof indicator.headerType === 'function') &&
+            (indicator as IImageHeaderIndicator).imageAutoSizing)
+        ) {
+          return true;
+        }
+      }
+    }
+  } else {
+    // list table
+    if (columns.length > 0) {
+      for (let i = 0; i < columns.length; i++) {
+        const column = columns[i] as unknown as ListTableConstructorOptions['columns'][0];
+        if (
+          ((column.cellType === 'image' || column.cellType === 'video' || typeof column.cellType === 'function') &&
+            (column as IImageColumnBodyDefine).imageAutoSizing) ||
+          ((column.headerType === 'image' ||
+            column.headerType === 'video' ||
+            typeof column.headerType === 'function') &&
+            (column as IImageHeaderDefine).imageAutoSizing)
+        ) {
+          return true;
+        }
+      }
     }
   }
   return false;
