7a7b76fef833435f6ba1fce720d7572a57fcf63f
Rui-Sun <sunrui1992@foxmail.com>
2024-05-31 17:19:37 +0800

    fix: add add breakString maxCharactersNumber judgement

--- packages/vtable/src/scenegraph/utils/break-string.ts
+++ packages/vtable/src/scenegraph/utils/break-string.ts
@@ -1,7 +1,12 @@
+import { isString } from '@visactor/vutils';
 import { convertInternal } from '../../tools/util';
 import type { BaseTableAPI } from '../../ts-types/base-table';
 
 export function breakString(textStr: string, table: BaseTableAPI): string[] {
+  if (isString(textStr) && textStr.length > (table.options.maxCharactersNumber || 200)) {
+    textStr = textStr.slice(0, table.options.maxCharactersNumber || 200);
+    textStr += '\u2026';
+  }
   let text;
   if (!table.internalProps.enableLineBreak && !table.options.customConfig?.multilinesForXTable) {
     text = [convertInternal(textStr)];
