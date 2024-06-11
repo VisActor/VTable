7cc7799285fd6175480ca5e0e75665643ab4af5f
Rui-Sun <sunrui1992@foxmail.com>
2024-06-11 14:45:30 +0800

    chore: add rollup-plugin-visualizer

--- packages/vtable/bundler.config.js
+++ packages/vtable/bundler.config.js
@@ -1,14 +1,24 @@
 /**
  * @type {Partial<import('@internal/bundler').Config>}
  */
+const { visualizer } = require("rollup-plugin-visualizer");
+
 module.exports = {
-  formats: ['cjs', 'es', 'umd'],
+  formats: ['umd'],
   noEmitOnError: false,
   copy: ['css'],
   name: 'VTable',
   umdOutputFilename: 'vtable',
   rollupOptions: {
-    treeshake: true
+    treeshake: true,
+    plugins: [
+      visualizer({
+        filename: 'report.html',
+        open: true,
+        gzipSize: true,
+        brotliSize: true,
+    })
+    ]
   },
   globals: {
     // '@visactor/vtable-editors': 'VTable.editors'
