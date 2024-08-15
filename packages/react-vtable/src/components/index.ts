7d63f8948264348439665119924c544866116d6a
Rui-Sun <sunrui1992@foxmail.com>
2024-08-15 19:39:02 +0800

    feat: add components in react-vtable

--- packages/react-vtable/src/components/index.ts
+++ packages/react-vtable/src/components/index.ts
@@ -2,3 +2,5 @@ export * from './vrender-components/tag';
 export * from './vrender-components/checkbox';
 export * from './vrender-components/radio';
 export * from './button/button';
+export * from './link/link';
+export * from './avatar/avatar';
