eea33f11263e855821f82ffd6127e19ef49151d5
fangsmile <892739385@qq.com>
2024-08-21 10:12:37 +0800

    fix: bugserver build error

--- tools/bugserver-trigger/bundler.config.js
+++ tools/bugserver-trigger/bundler.config.js
@@ -1,12 +1,28 @@
 /**
  * @type {Partial<import('@internal/bundler').Config>}
  */
+
+const alias = require('@rollup/plugin-alias');
+
+const path = require('path');
+
 module.exports = {
   formats: ['umd'],
   // name: 'VTable',
 
   umdOutputFilename: 'index',
   minify: false,
+  rollupOptions: {
+    treeshake: true,
+    plugins: [
+      alias({
+        entries: [
+          { find: '@visactor/vtable/es/vrender', replacement: path.resolve(__dirname, '../../packages/vtable/es/vrender') }
+        ]
+      }),
+      // 其他插件
+    ]
+  },
   output: {
     footer: '/* follow me on Twitter! @rich_harris */'
   }
