fcb38ffa99cc9b0f2f8de48be6051f2e26ac5084
Rui-Sun <sunrui1992@foxmail.com>
2024-08-26 16:56:55 +0800

    fix: add judgement in array find function #2289

--- packages/vtable/src/ListTable.ts
+++ packages/vtable/src/ListTable.ts
@@ -375,6 +375,9 @@ export class ListTable extends BaseTable implements ListTableAPI {
   }
   getTableIndexByField(field: FieldDef) {
     const colObj = this.internalProps.layoutMap.columnObjects.find((col: any) => col.field === field);
+    if (!colObj) {
+      return undefined;
+    }
     const layoutRange = this.internalProps.layoutMap.getBodyLayoutRangeById(colObj.id);
     if (this.transpose) {
       return layoutRange.start.row;
@@ -957,7 +960,7 @@ export class ListTable extends BaseTable implements ListTableAPI {
       const sortFunc = this._getSortFuncFromHeaderOption(this.internalProps.columns, field);
       const hd = this.internalProps.layoutMap.headerObjects.find((col: any) => col && col.field === field);
 
-      if (hd.define.sort !== false) {
+      if (hd && hd.define.sort !== false) {
         this.dataSource.sort(hd.field, order, sortFunc);
 
         // clear cell range cache
@@ -1074,7 +1077,7 @@ export class ListTable extends BaseTable implements ListTableAPI {
             (col: any) => col && col.field === field
           );
           // hd?.define?.sort && //如果这里也判断 那想要利用sortState来排序 但不显示排序图标就实现不了
-          if (hd.define.sort !== false) {
+          if (hd && hd.define.sort !== false) {
             this.dataSource.sort(hd.field, order, sortFunc ?? defaultOrderFn);
           }
         }
