79c9ebe774b2e7c7a0c97996773ae4836a488d62
YEL!ne <1638317920@qq.com>
2024-09-02 01:37:41 +0800

    fix:  custom layout for grammatical tag handling

--- packages/vue-vtable/src/components/index.ts
+++ packages/vue-vtable/src/components/index.ts
@@ -10,4 +10,10 @@ export { default as PivotCorner } from './pivot/pivot-corner';
 export { default as Menu } from './component/menu';
 export { default as Tooltip } from './component/tooltip';
 
-export { default as CustomLayout } from './custom/custom-layout';
+// export { default as CustomLayout } from './custom/custom-layout';
+export { default as Group } from './custom/group';
+export { default as Image } from './custom/image';
+export { default as Text } from './custom/text';
+export { default as Tag } from './custom/tag';
+export { default as Radio } from './custom/radio';
+export { default as CheckBox } from './custom/checkBox';
