61a4b344735345e9488ff07ce69b009cebd4fd6b
Rui-Sun <sunrui1992@foxmail.com>
2024-09-29 16:50:30 +0800

    feat: add scrollTo animation API

--- packages/vtable/src/core/animation.ts
+++ packages/vtable/src/core/animation.ts
@@ -1,13 +1,23 @@
-import type { IRect } from 'src/vrender';
-import { DefaultTimeline, DefaultTicker, Animate, ACustomAnimate, createRect } from '@visactor/vrender-core';
+import type { EasingType, IRect } from '@src/vrender';
+import { DefaultTimeline, DefaultTicker, Animate, ACustomAnimate, createRect } from '@src/vrender';
 import type { BaseTableAPI } from '../ts-types/base-table';
+import { isBoolean, isNumber } from '@visactor/vutils';
+
+export type ITableAnimationOption = {
+  duration?: number;
+  easing?: EasingType;
+};
 
 class Animateaaa extends ACustomAnimate<any> {
   onUpdate(end: boolean, ratio: number, out: Record<string, any>): void {
-    // debugger;
-    // console.log(end, ratio, out);
-    const y = end ? this.to.y : this.from.y + Math.floor((this.to.y - this.from.y) * ratio);
-    this.params.table.scrollTop = y;
+    if (this.from.x !== this.to.x) {
+      const x = end ? this.to.x : this.from.x + Math.floor((this.to.x - this.from.x) * ratio);
+      this.params.table.scrollLeft = x;
+    }
+    if (this.from.y !== this.to.y) {
+      const y = end ? this.to.y : this.from.y + Math.floor((this.to.y - this.from.y) * ratio);
+      this.params.table.scrollTop = y;
+    }
   }
 }
 
@@ -23,23 +33,32 @@ export class TableAnimationManager {
     this.ticker = new DefaultTicker([this.timeline]);
     // no use, for avoid error in vrender animation
     this.tempGraphic = createRect({});
-    // this.animation = new Animate().bind(this.tempGraphic);
   }
 
-  scrollToRow(row: number) {
+  scrollTo(position: { col?: number; row?: number }, animationOption?: ITableAnimationOption | true) {
     const from = {
+      x: this.table.scrollLeft,
       y: this.table.scrollTop
     };
+    const cellRect = this.table.getCellRect(position.col ?? 0, position.row ?? 0);
     const to = {
-      y: this.table.getCellRect(0, row).top
+      x: isNumber(position.col) ? cellRect.left - this.table.getFrozenColsWidth() : this.table.scrollLeft,
+      y: isNumber(position.row) ? cellRect.top - this.table.getFrozenRowsHeight() : this.table.scrollTop
     };
+    const duration = (!isBoolean(animationOption) && animationOption?.duration) ?? 3000;
+    const easing = (!isBoolean(animationOption) && animationOption?.easing) ?? 'linear';
     const animation = new Animate().bind(this.tempGraphic).play(
-      new Animateaaa(from, to, 3000, 'linear', {
+      new Animateaaa(from, to, duration, easing, {
         graphic: this.tempGraphic,
         table: this.table
       })
     );
     this.timeline.addAnimate(animation);
     this.ticker.start();
+  }
+
+  clear() {
+    this.timeline.clear();
+    this.ticker.stop();
   }
 }
