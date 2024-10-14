b76dcaa07af156d479b9342b24d5d59a1f222f6f
Rui-Sun <sunrui1992@foxmail.com>
2024-10-14 19:34:22 +0800

    fix: fix CONTEXTMENU auto hide

--- packages/vtable/src/event/listener/table-group.ts
+++ packages/vtable/src/event/listener/table-group.ts
@@ -667,7 +667,10 @@ export function bindTableGroupListener(eventManager: EventManager) {
   // stage 的pointerdown监听
   table.scenegraph.stage.addEventListener('pointerdown', (e: FederatedPointerEvent) => {
     const eventArgsSet: SceneEvent = getCellEventArgsSet(e);
-    if ((eventArgsSet.eventArgs?.target as any) !== stateManager.residentHoverIcon?.icon) {
+    if (
+      !eventArgsSet.eventArgs?.target ||
+      (eventArgsSet.eventArgs?.target as any) !== stateManager.residentHoverIcon?.icon
+    ) {
       stateManager.hideMenu();
     }
     (table as ListTableAPI).editorManager?.completeEdit(e.nativeEvent);
