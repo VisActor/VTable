b5af1cce7cf1c6ddaa42565d14ab3b9c3f545384
Rui-Sun <sunrui1992@foxmail.com>
2024-10-18 11:42:45 +0800

    feat: add title click handler in calendar

--- packages/vtable-calendar/src/month-calendar.ts
+++ packages/vtable-calendar/src/month-calendar.ts
@@ -1,11 +1,10 @@
-import type { TYPES } from '@visactor/vtable';
 import { ListTable, themes, CustomLayout } from '@visactor/vtable';
 import type { DateRecord, DateRecordKeys } from './date-util';
 import { defaultDayTitles, getMonthString, getRecords, getStartAndEndDate, getWeekdayString } from './date-util';
 import { bindDebugTool } from '../../vtable/src/scenegraph/debug-tool';
 import { add, differenceInDays, lastDayOfMonth, previousSunday } from 'date-fns';
 import { getMonthCustomStyleRange } from './style';
-import type { CellRange, CustomRenderFunctionArg, ITableAnimationOption } from '@visactor/vtable/es/ts-types';
+import type { CellRange, ITableAnimationOption } from '@visactor/vtable';
 import { createTableOption } from './table/table-option';
 
 interface VTableCalendarConstructorOptions {
@@ -18,7 +17,7 @@ interface VTableCalendarConstructorOptions {
   // rowHeight?: number;
   dayTitles?: [string, string, string, string, string, string, string];
 
-  style: {
+  style?: {
     headerRowHeight?: number;
   };
 }
@@ -36,6 +35,8 @@ export class VTableCalendar {
   currentMonthCellRanges: { range: CellRange }[];
   currentMonth: number;
   currentYear: number;
+
+  titleClickHandler: () => void;
 
   constructor(container: HTMLElement, options?: VTableCalendarConstructorOptions) {
     this.container = container;
@@ -57,168 +58,18 @@ export class VTableCalendar {
     }
     this.tableStartDate = this.startDate.getDay() === 0 ? this.startDate : previousSunday(this.startDate);
 
-    this.createTable();
+    this._createTable();
+
+    this._bindEvent();
   }
 
-  createTable() {
-    const { columnWidth, rowHeight, headerRowHeight, dayTitles } = this.options;
+  _createTable() {
+    const { dayTitles } = this.options;
 
     const records = getRecords(this.startDate, this.endDate);
     this.records = records;
 
-    // const columnWidth = 140;
     const week = (dayTitles ?? defaultDayTitles) as DateRecordKeys[];
-    // const columns = week.map((item: DateRecordKeys, index) => {
-    //   return {
-    //     field: defaultDayTitles[index],
-    //     title: item,
-    //     width: columnWidth ?? 140,
-    //     fieldFormat: (record: DateRecord) => {
-    //       if (
-    //         record.year === this.currentDate.getFullYear() &&
-    //         record.month === this.currentDate.getMonth() &&
-    //         record[item] === this.currentDate.getDate()
-    //       ) {
-    //         return `${record[item]}\nToday`;
-    //       } else if (record[item] === 1) {
-    //         const monthIndex = item === 'Sun' ? record.month : record.month + 1;
-    //         const mouthStr = getMonthString(monthIndex);
-    //         return `${record[item]}\n${mouthStr}`;
-    //       }
-    //       return record[item];
-    //     }
-    //     // customLayout: (args: CustomRenderFunctionArg) => {
-    //     //   const { table, row, col, rect, value } = args;
-    //     //   const record = table.getRecordByCell(col, row);
-    //     //   const { height, width } = rect ?? table.getCellRect(col, row);
-
-    //     //   const container = new CustomLayout.Group({
-    //     //     x: 0,
-    //     //     y: 0,
-    //     //     height,
-    //     //     width
-    //     //   });
-
-    //     //   // container.onBeforeAttributeUpdate = attr => {
-    //     //   //   if (attr.y) {
-    //     //   //     debugger;
-    //     //   //   }
-    //     //   // };
-
-    //     //   if (value === 12) {
-    //     //     const text = new CustomLayout.Text({
-    //     //       x: 25,
-    //     //       y: 50,
-    //     //       text: 'Event A',
-    //     //       fontSize: 15,
-    //     //       fill: '#f99',
-    //     //       textAlign: 'left',
-    //     //       textBaseline: 'middle'
-    //     //     });
-    //     //     const circle = new CustomLayout.Circle({
-    //     //       x: 15,
-    //     //       y: 50,
-    //     //       radius: 5,
-    //     //       fill: '#f99'
-    //     //     });
-    //     //     container.add(text);
-    //     //     container.add(circle);
-    //     //   } else if (value === 21) {
-    //     //     const text = new CustomLayout.Text({
-    //     //       x: 20,
-    //     //       y: 54 - 20,
-    //     //       text: 'Event B',
-    //     //       fontSize: 15,
-    //     //       fill: '#fff',
-    //     //       textAlign: 'left',
-    //     //       textBaseline: 'middle'
-    //     //     });
-    //     //     const rect = new CustomLayout.Rect({
-    //     //       x: 5,
-    //     //       y: 45 - 20,
-    //     //       width: 130,
-    //     //       height: 20,
-    //     //       fill: '#f99',
-    //     //       cornerRadius: 5
-    //     //     });
-    //     //     container.add(rect);
-    //     //     container.add(text);
-
-    //     //     const text1 = new CustomLayout.Text({
-    //     //       x: 20,
-    //     //       y: 54 + 10,
-    //     //       text: 'Event C',
-    //     //       fontSize: 15,
-    //     //       fill: '#fff',
-    //     //       textAlign: 'left',
-    //     //       textBaseline: 'middle'
-    //     //     });
-    //     //     const rect1 = new CustomLayout.Rect({
-    //     //       x: 5,
-    //     //       y: 45 + 10,
-    //     //       width: 130,
-    //     //       height: 20,
-    //     //       fill: '#9f9',
-    //     //       cornerRadius: 5
-    //     //     });
-    //     //     container.add(rect1);
-    //     //     container.add(text1);
-    //     //   }
-
-    //     //   return {
-    //     //     rootContainer: container,
-    //     //     renderDefault: true
-    //     //   };
-    //     // }
-    //   };
-    // });
-    // const option: TYPES.ListTableConstructorOptions = {
-    //   container: this.container,
-    //   records,
-    //   columns,
-    //   defaultRowHeight: rowHeight ?? 120,
-    //   defaultHeaderRowHeight: headerRowHeight ?? 40,
-    //   theme: themes.DEFAULT.extends({
-    //     headerStyle: {
-    //       textAlign: 'center'
-    //     },
-    //     bodyStyle: {
-    //       bgColor: args => {
-    //         const { col, row, dataValue, table } = args;
-    //         const record = table.getCellRawRecord(col, row);
-    //         if (
-    //           record.year === this.currentDate.getFullYear() &&
-    //           record.month === this.currentDate.getMonth() &&
-    //           dataValue === this.currentDate.getDate()
-    //         ) {
-    //           return '#f0f0f0';
-    //         }
-    //         return '#fff';
-    //       },
-    //       textAlign: 'right',
-    //       textBaseline: 'top',
-    //       color: '#999'
-    //     }
-    //   }),
-    //   title: {
-    //     orient: 'top',
-    //     // text: 'Thu, Aug 22',
-    //     text: `${getWeekdayString(this.currentDate.getDay())}, ${getMonthString(
-    //       this.currentDate.getMonth()
-    //     )} ${this.currentDate.getDate()}`,
-    //     subtext: this.currentDate.getFullYear()
-    //   },
-    //   enableLineBreak: true,
-    //   customCellStyle: [
-    //     {
-    //       id: 'current-month',
-    //       style: {
-    //         color: '#000'
-    //       }
-    //     }
-    //   ]
-    // };
-
     const option = createTableOption(week, this.currentDate, {
       style: this.options.style,
       containerWidth: this.container.clientWidth,
@@ -300,5 +151,31 @@ export class VTableCalendar {
     this.currentMonthCellRanges.forEach(range => {
       this.table.arrangeCustomCellStyle(range, 'current-month');
     });
+  }
+
+  _bindEvent() {
+    // click title jump to current month
+    const titleComponent = this.table.internalProps.title.getComponentGraphic();
+    titleComponent.setAttributes({
+      cursor: 'pointer',
+      // hack for cursor
+      childrenPickable: false
+    });
+    this.titleClickHandler = (() => {
+      this.jumpToCurrentMonth({
+        duration: 500
+      });
+    }).bind(this);
+    titleComponent.addEventListener('click', this.titleClickHandler);
+  }
+
+  _unbindEvent() {
+    const titleComponent = this.table.internalProps.title.getComponentGraphic();
+    titleComponent.removeEventListener('click', this.titleClickHandler);
+  }
+
+  release() {
+    // unbind event
+    this._unbindEvent();
   }
 }
