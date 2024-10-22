57d292432a37152e20086e60255389cd9861c6ee
Rui-Sun <sunrui1992@foxmail.com>
2024-10-22 14:59:03 +0800

    fix: disable cellInnerBorder when no frame border

--- packages/vtable/src/tools/style.ts
+++ packages/vtable/src/tools/style.ts
@@ -1,3 +1,4 @@
+import { isArray, isValid } from '@visactor/vutils';
 import type { ScrollStyle } from '../ts-types';
 
 export function getScrollBarSize(scrollStyle?: ScrollStyle): number {
@@ -5,4 +6,19 @@ export function getScrollBarSize(scrollStyle?: ScrollStyle): number {
     return 0;
   }
   return scrollStyle?.width ?? 7;
+}
+
+export function isValidStyle(style: (string | number) | (string | number)[]) {
+  if (!isValid(style)) {
+    return false;
+  }
+  if (isArray(style)) {
+    return style.some(s => isValid(s));
+  }
+
+  return true;
+}
+
+export function isZeroStyle(style: number | number[]) {
+  return style === 0 || (isArray(style) && style.every(s => s === 0));
 }
