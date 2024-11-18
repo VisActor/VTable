ed5d72cd66cf5d2ca4750b5794d2fb2611d4c8e6
Rui-Sun <sunrui1992@foxmail.com>
2024-11-18 21:52:18 +0800

    feat: isShowOverflowTextTooltip support function

--- packages/vtable/src/ts-types/base-table.ts
+++ packages/vtable/src/ts-types/base-table.ts
@@ -206,7 +206,7 @@ export interface IBaseTableProtected {
     parentElement: HTMLElement;
     renderMode: 'html' | 'canvas';
     /** 代替原来hover:isShowTooltip配置 */
-    isShowOverflowTextTooltip: boolean;
+    isShowOverflowTextTooltip: boolean | ((col: number, row: number, table: BaseTableAPI) => boolean);
     /** 缩略文字提示框 延迟消失时间 */
     overflowTextTooltipDisappearDelay?: number;
     /** 弹框是否需要限定在表格区域内 */
@@ -397,7 +397,7 @@ export interface BaseTableConstructorOptions {
     /** html目前实现较完整 先默认html渲染方式 */
     renderMode?: 'html'; // 目前暂不支持canvas方案
     /** 是否显示缩略文字提示框。 代替原来hover:isShowTooltip配置 暂时需要将renderMode配置为html才能显示，canvas的还未开发*/
-    isShowOverflowTextTooltip?: boolean;
+    isShowOverflowTextTooltip?: boolean | ((col: number, row: number, table: BaseTableAPI) => boolean);
     /** 缩略文字提示框 延迟消失时间 */
     overflowTextTooltipDisappearDelay?: number;
     /** 是否将 tooltip 框限制在画布区域内，默认开启。针对renderMode:"html"有效 */
