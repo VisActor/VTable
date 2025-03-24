75941ac0bf76c8fae82587ff86b8f18b199e31c8
lixuefei.1313 <lixuef1313@163.com>
2025-03-24 19:02:10 +0800

    feat: add chart picker

---
+++ packages/vtable/src/scenegraph/graphic/contributions/vchart-graphic-picker.ts
@@ -0,0 +1,33 @@
+import { injectable } from '@src/vrender';
+import type { IGraphicPicker, IPickParams } from '@src/vrender';
+import type { Chart as VChartGraphic } from '../chart';
+import { CHART_NUMBER_TYPE } from '../chart';
+
+@injectable()
+export class VChartPicker implements IGraphicPicker {
+  type = 'chart';
+  numberType: number = CHART_NUMBER_TYPE;
+
+  contains(chart: any, point: any, params?: IPickParams): boolean | any {
+    // 将当前的point转化到global
+    const matrix = chart.parent.globalTransMatrix.clone();
+    const stageMatrix = chart.stage.window.getViewBoxTransform();
+    matrix.multiply(stageMatrix.a, stageMatrix.b, stageMatrix.c, stageMatrix.d, stageMatrix.e, stageMatrix.f);
+    const toGlobalMatrix = matrix.getInverse();
+    const nextP = { x: 0, y: 0 };
+    toGlobalMatrix.transformPoint(point, nextP);
+
+    // 得到 vchart stage
+    const vChart = (chart as VChartGraphic).activeChartInstance;
+    const vchartStage = vChart.getStage();
+    vchartStage.dirtyBounds?.clear();
+    const toChartMatrix = vchartStage.window.getViewBoxTransform();
+    toChartMatrix.transformPoint(nextP, nextP);
+    const pick = vchartStage.pick(nextP.x, nextP.y);
+    // @ts-ignore
+    if (pick.graphic === null && pick.group.name === 'root') {
+      return false;
+    }
+    return pick;
+  }
+}
