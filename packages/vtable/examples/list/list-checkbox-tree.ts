a82bfa9222417e5a8e746f95995211e141a8185f
TianYi <2739022972@qq.com>
2025-05-14 21:14:53 +0800

    feat: 更新cellType: 'checkbox'与tree: true的demo

--- packages/vtable/examples/list/list-checkbox-tree.ts
+++ packages/vtable/examples/list/list-checkbox-tree.ts
@@ -147,6 +147,8 @@ export function createTable() {
       {
         field: '类别',
         tree: true,
+        cellType: 'checkbox',
+        enableTreeCheckbox: true,
         title: '类别',
         width: 'auto',
         sort: true
