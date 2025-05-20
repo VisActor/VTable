64f6b6296f538465196360f3e6d2154ecbc4428e
lixuefei.1313 <lixuef1313@163.com>
2025-05-20 19:42:27 +0800

    fix: fix bug of editor manager

--- packages/vtable/src/PivotChart.ts
+++ packages/vtable/src/PivotChart.ts
@@ -73,7 +73,8 @@ import {
   registerVideoCell
 } from './scenegraph/group-creater/cell-type';
 import { hasLinearAxis } from './layout/chart-helper/get-axis-config';
-import { cacheStageCanvas } from './scenegraph/graphic/contributions/chart-render-helper';
+import { cacheStageCanvas, clearChartRenderQueue } from './scenegraph/graphic/contributions/chart-render-helper';
+import { EditManager } from './edit/edit-manager';
 
 registerAxis();
 registerEmptyTip();
@@ -265,6 +266,11 @@ export class PivotChart extends BaseTable implements PivotChartAPI {
     );
 
     this.internalProps.layoutMap = new PivotHeaderLayoutMap(this, this.dataset, columnDimensionTree, rowDimensionTree);
+
+    if (Env.mode !== 'node') {
+      this.editorManager = new EditManager(this);
+    }
+
     this.refreshHeader();
     this.internalProps.useOneRowHeightFillAll = false;
     // this.internalProps.frozenColCount = this.options.frozenColCount || this.rowHeaderLevelCount;
