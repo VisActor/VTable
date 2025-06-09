45d3f38b000b53fa417624320f1c80dacf400ad7
fangsmile <892739385@qq.com>
2025-06-09 17:58:06 +0800

    feat: init sheet table

--- packages/vtable-sheet/src/index.ts
+++ packages/vtable-sheet/src/index.ts
@@ -1,9 +1,8 @@
 import VTableSheet from './components/VTableSheet';
-import SheetManager from './data/SheetManager';
-import { FormulaManager } from './data/FormulaManager';
-import { FilterManager } from './data/FilterManager';
+import SheetManager from './managers/SheetManager';
+import { FormulaManager } from './managers/FormulaManager';
+import { FilterManager } from './managers/FilterManager';
 import * as tools from './tools';
-import * as plugins from './plugins';
 import type { SheetDefine, VTableSheetOptions } from './ts-types';
 
 export const version = '1.0.0';
@@ -11,7 +10,7 @@ export const version = '1.0.0';
 /**
  * @namespace VTableSheet
  */
-export { tools, plugins, VTableSheet, SheetManager, FormulaManager, FilterManager };
+export { tools, VTableSheet, SheetManager, FormulaManager, FilterManager };
 
 export type { SheetDefine, VTableSheetOptions };
 
--- packages/vtable-sheet/src/plugins/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * 插件导出
- * @namespace plugins
- */
-
-// 暂时没有导出的插件
-export {};
