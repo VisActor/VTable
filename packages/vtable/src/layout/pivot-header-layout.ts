80e9a9c56fa2bdb2e863186a040f5ac9b5d91b78
fangsmile <892739385@qq.com>
2025-12-19 14:59:49 +0800

    fix: hasLeftIndicatorAxis judgement logic

--- packages/vtable/src/layout/pivot-header-layout.ts
+++ packages/vtable/src/layout/pivot-header-layout.ts
@@ -1769,7 +1769,7 @@ export class PivotHeaderLayoutMap implements LayoutMapAPI {
       //#endregion
       if (
         !this.indicatorsAsCol &&
-        (this.hideIndicatorName || !this.hasLeftIndicatorAxis) &&
+        (this.hideIndicatorName || (!this.hasLeftIndicatorAxis && this._table.isPivotChart())) &&
         this.rowDimensionKeys[this.rowDimensionKeys.length - 1] === this.indicatorDimensionKey
       ) {
         count = rowLevelCount - 1;
