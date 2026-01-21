c9dee2b4698bb1b9bb6b5eed7743a52b4bc1cfe3
youli <1418591636@qq.com>
2026-01-21 08:55:32 +0800

    feat: add cancelEditCell method to ListTable and PivotTable APIs

--- packages/vtable/src/PivotTable.ts
+++ packages/vtable/src/PivotTable.ts
@@ -1753,6 +1753,10 @@ export class PivotTable extends BaseTable implements PivotTableAPI {
   completeEditCell() {
     this.editorManager.completeEdit();
   }
+  /** 取消编辑 */
+  cancelEditCell() {
+    this.editorManager.cancelEdit();
+  }
   /** 获取单元格对应的编辑器 */
   getEditor(col: number, row: number) {
     const lastSelectedCellEditor = this.editorManager.cacheLastSelectedCellEditor[`${col}-${row}`];
